27596f5841fa1dfff4157fc586339f93b94f2e61
test <1964960027@qq.com>
2025-09-17 10:25:58 +0800

    2025.9.16

---
+++ l2070/src/Solution.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+/**
+ * 二分算法
+ * 需要注意while的判断条件 判断条件均是区间内没有元素
+ * 区间[] [) ()的选择
+ */
+public class Solution {
+    public int[] maximumBeauty(int[][] items, int[] queries) {
+
+    }
+
+    // []
+    private int lowerBound(int[] nums, int left, int target) {
+
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.countFairPairs(new int[]{0,1,7,4,4,5}, 3, 6));
+    }
+}
--- l2563/src/Solution.java
+++ l2563/src/Solution.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * 二分算法
  * 需要注意while的判断条件 判断条件均是区间内没有元素
@@ -5,11 +7,32 @@
  */
 public class Solution {
     public long countFairPairs(int[] nums, int lower, int upper) {
+        long ans = 0;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            int l = lowerBound(nums, i, lower - nums[i]);
+            int r = lowerBound(nums, i, upper - nums[i] + 1);
+            ans += r - l;
+        }
+        return ans;
+    }
 
+    // ()
+    private int lowerBound(int[] nums, int left, int target) {
+        int right = nums.length;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        return right;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.nextGreatestLetter(new char[]{'c', 'f', 'j'}, 'a'));
+        System.out.println(solution.countFairPairs(new int[]{0,1,7,4,4,5}, 3, 6));
     }
 }
