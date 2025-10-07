feeeffbb0bdea359817c40610d47f6784058e32f
test <1964960027@qq.com>
2025-10-07 20:34:22 +0800

    2025.10.7

--- l3007/src/Solution.java
+++ l3007/src/Solution.java
@@ -5,27 +5,56 @@ class Solution {
      *  数位dp + 二分
      */
     public long findMaximumNumber(long k, int x) {
+        this.x = x;
+        long left = 0;
+        // 上界怎么设置？
+        long right = (k + 1) << x;
+        while (left + 1 < right) {
+            long mid = left + (right - left) / 2;
+            if (check(mid, k)) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
 
+    private long num;
+    private long x;
+    private long memo[][];
+
+    private boolean check(long mid, long k) {
+        this.num = mid;
+        // 计算mid的二进制数有多少位
+        // Long.numberOfLeadingZeros(mid)计算mid二进制数有多少前导0
+        int m = 64 - Long.numberOfLeadingZeros(mid);
+        memo = new long[m][m + 1];
+        for (long[] row : memo) {
+            Arrays.fill(row, -1);
+        }
+        long ans = dfs(m, 0, true);
+        return ans <= k;
     }
 
     /**
      * 数位dp函数模版
-     * @param i 现在是字符串的第i位数
+     * @param i 现在是num的第i位二进制数
      * @param mask 用来计算1的个数
      * @param isLimit 判断是否是上一个位数的最大值，如果是最大值，那么下一位就会被限制
      */
-    private int dfs(int i, int mask, boolean isLimit, char[] s, int[][] memo) {
-        // 已经dfs完了，返回1的个数
-        if (i == s.length) return mask;
-        // 如果第n位没限制时可以加入多少个
+    private long dfs(int i, int mask, boolean isLimit) {
+        // 如果算到头了，返回mask
+        if (i <= 0) return mask;
+        // 如果之前计算过了，就不重复计算了
         if (!isLimit && memo[i][mask] != -1) {
             return memo[i][mask];
         }
-        // 如果该位置有限制
-        int res = 0;
-        int up = isLimit ? s[i] - '0' : 9;
+
+        long res = 0;
+        int up = isLimit ? (int)((num >> (i - 1)) & 1) : 1;
         for (int d = 0; d <= up; d++) {
-            res += dfs(i + 1, d == 1 ? mask + 1 : mask, isLimit && (d == up), s, memo);
+            res += dfs(i - 1, (d == 1 && i % x == 0) ? mask + 1 : mask, isLimit && d == up);
         }
 
         if (!isLimit && memo[i][mask] == -1) {
@@ -36,6 +65,6 @@ class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.countDigitOne(824883294));
+        System.out.println(solution.findMaximumNumber(7, 2));
     }
 }
